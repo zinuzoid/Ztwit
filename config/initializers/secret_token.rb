dbd2666f133bcb7770ead470d2c3264aa0fbedfd
Jetsada Machom <zinuzoid@gmail.com>
2011-11-13 18:34:17 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ztwit::Application.config.secret_token = '055c038b83a7dbf5d81d1ddf55ed2f72e2028c21af7c4f3d7bd2fdea035aff379e932c365c2ff89a9b77646cf38a294015432e5c59e763d1a371f26a21ebb060'
